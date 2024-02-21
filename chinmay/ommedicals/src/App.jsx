51cc1ef5a1257b447da83b4cb22031adfd3c16d6
Chinmay <chinmaybarik214@gmail.com>
2024-02-21 23:45:01 +0530

    pulled product store code from hackcom proj

--- chinmay/ommedicals/src/App.jsx
+++ chinmay/ommedicals/src/App.jsx
@@ -6,7 +6,138 @@ function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <div class="container">
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img src="images/item1.webp" />
+                </div>
+                <h4>St. Moritz Reversible Jacket</h4>
+                <span>La DoubleJ</span>
+                <span>₹1450 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img src="images/item2.webp" />
+                </div>
+                <h4>Hollis Dickey Jacket</h4>
+                <span>Veronica Beard</span>
+                <span>₹598 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img src="images/item3.webp" />
+                </div>
+                <h4>Hazel Sweater</h4>
+                <span>Rachel Antonoff</span>
+                <span>₹294 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img src="images/item4.webp" />
+                </div>
+                <h4>Bow Drop Earrings</h4>
+                <span>Lele Sadoughi</span>
+                <span>₹145 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img src="images/item5.webp" />
+                </div>
+                <h4>Dolly Fringe Turtleneck</h4>
+                <span>Central Park West</span>
+                <span>₹152 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img src="images/item6.webp" />
+                </div>
+                <h4>Nico Blazer</h4>
+                <span>Central Park West</span>
+                <span>₹260 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img src="images/item7.webp" />
+                </div>
+                <h4>Odense Jacket</h4>
+                <span>Elk</span>
+                <span>₹315 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img src="images/item8.webp" />
+                </div>
+                <h4>Star Dust Sweater</h4>
+                <span>Perfect Moment</span>
+                <span>₹400 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img src="images/item9.webp" />
+                </div>
+                <h4>Royce Pullover</h4>
+                <span>Anine Bing</span>
+                <span>₹349 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img src="images/item10.webp" />
+                </div>
+                <h4>Lennox Bag</h4>
+                <span>Loeffler Randall</span>
+                <span>₹295 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
+    </div>
     </>
   )
 }
