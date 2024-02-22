1e5b44d15fa310e1c7088910f0cb548ea794fd25
Chinmay <chinmaybarik214@gmail.com>
2024-02-22 20:30:03 +0530

    changed classes to classNames

--- chinmay/ommedicals/src/Product.jsx
+++ chinmay/ommedicals/src/Product.jsx
@@ -5,17 +5,17 @@ import './Product.css'
 function Product({id, title, image, price, alt, category}) {
     return (
     <>
-        <div class="height product">
-            <div class="prod-desc">
-                <div class="prod-img-container">
+        <div className="height product">
+            <div className="prod-desc">
+                <div className="prod-img-container">
                 <img src={image} />
                 </div>
-                <h4 class="ellipses">{title}</h4>
-                <span class="ellipses">{category}</span>
-                <span class="ellipses">₹{price} to buy</span>
+                <h4 className="ellipses">{title}</h4>
+                <span className="ellipses">{category}</span>
+                <span className="ellipses">₹{price} to buy</span>
             </div>
-            <div class="prod-btn-container">
-                <button class="prod-btn">Add to cart</button>
+            <div className="prod-btn-container">
+                <button className="prod-btn">Add to cart</button>
             </div>
         </div>
     </>
