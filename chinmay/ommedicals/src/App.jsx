e806344a41e553cb4b23adbd96c8f80572ddd50f
Chinmay <chinmaybarik214@gmail.com>
2024-02-28 10:38:10 +0530

    revert

--- chinmay/ommedicals/src/App.jsx
+++ chinmay/ommedicals/src/App.jsx
@@ -1,6 +1,28 @@
 import './App.css'
 import Product from './Product'
 
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+initializeApp();
+const db = getFirestore();
+const docRef = db.collection('users').doc('alovelace');
+await docRef.set({
+  first: 'Ada',
+  last: 'Lovelace',
+  born: 1815
+});
+const aTuringRef = db.collection('users').doc('aturing');
+
+await aTuringRef.set({
+  'first': 'Alan',
+  'middle': 'Mathison',
+  'last': 'Turing',
+  'born': 1912
+});
+const snapshot = await db.collection('users').get();
+snapshot.forEach((doc) => {
+    console.log(doc.id, '=>', doc.data());
+});
 function App() {
     // JS scripts
     return (
