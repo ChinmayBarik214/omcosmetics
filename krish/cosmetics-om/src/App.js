27e48f9d08c94f6a03f20f8f0bfa8f7bb1c14468
Krishpunia22 <143069339+Krishpunia22@users.noreply.github.com>
2024-02-29 12:55:48 +0530

    commit

--- krish/cosmetics-om/src/App.js
+++ krish/cosmetics-om/src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. hello dear
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ return (
+    <>
+      <div class="container">
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img height={200} src="https://imgur.com/0jX8vWG.jpg" />
+                </div>
+                <h4> scenery nature green</h4>
+                <span>La DoubleJ</span>
+                <span>₹1450 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img height={200} src="https://imgur.com/BYHpfRC.jpg" />
+                </div>
+                <h4>mountains nature scenery </h4>
+                <span>Veronica Beard</span>
+                <span>₹598 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img src="images/item3.webp" />
+                </div>
+                <h4>Hazel Sweater</h4>
+                <span>Rachel Antonoff</span>
+                <span>₹294 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img src="images/item4.webp" />
+                </div>
+                <h4>Bow Drop Earrings</h4>
+                <span>Lele Sadoughi</span>
+                <span>₹145 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img src="images/item5.webp" />
+                </div>
+                <h4>Dolly Fringe Turtleneck</h4>
+                <span>Central Park West</span>
+                <span>₹152 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img src="images/item6.webp" />
+                </div>
+                <h4>Nico Blazer</h4>
+                <span>Central Park West</span>
+                <span>₹260 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img src="images/item7.webp" />
+                </div>
+                <h4>Odense Jacket</h4>
+                <span>Elk</span>
+                <span>₹315 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img src="images/item8.webp" />
+                </div>
+                <h4>Star Dust Sweater</h4>
+                <span>Perfect Moment</span>
+                <span>₹400 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img src="images/item9.webp" />
+                </div>
+                <h4>Royce Pullover</h4>
+                <span>Anine Bing</span>
+                <span>₹349 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
+        <div class="height product">
+            <div class="prod-desc">
+                <div class="prod-img-container">
+                <img src="images/item10.webp" />
+                </div>
+                <h4>Lennox Bag</h4>
+                <span>Loeffler Randall</span>
+                <span>₹295 to buy</span>
+            </div>
+            <div class="prod-btn-container">
+                <button class="prod-btn">Add to cart</button>
+            </div>
+        </div>
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
